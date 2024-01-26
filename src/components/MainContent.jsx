02bb0e60256dc253e527d5ac52c5a3b183525a31
Edjere Evelyn <evelynedjere@gmail.com>
2024-01-26 13:04:19 +0100

    Fix styling issues and update responsive layout

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,45 @@
+// components/AnalyticsDashboard.jsx
+import React from 'react';
+import { useTheme } from '../context/ThemeContext';
+import { useEffect } from 'react';
+import SalesTrendGraph from './SalesTrendGraph';
+import TotalOrdersGraph from './TotalOrdersGraph';
+import TotalRefundGraph from './TotalRefundGraph';
+import AverageSales from './AverageSales';
+import TotalIncome from './TotalIncome';
+import LastOrdersTable from './LastOrdersTable';
+import TopPlatforms from './TopPlatforms';
+
+const AnalyticsDashboard = () => {
+  const { darkMode, toggleTheme } = useTheme();
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+  }, [darkMode]);
+  return (
+    
+      <div className={`bg-[#fafafa]   text-black lg:w-full ${darkMode ? 'bg-black text-white' : 'bg-white text-black'}`}>
+
+        <div className='mx-4 mt-5'>
+        <div className="grid sm:grid-cols-1  lg:grid-cols-2 gap-4">
+          <div className='sticky'>
+          <SalesTrendGraph />
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-4">
+          <TotalOrdersGraph />
+          <TotalRefundGraph />
+          <AverageSales />
+          <TotalIncome />
+          </div>
+          
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-2">
+          <LastOrdersTable />
+          <TopPlatforms />
+          </div>
+          </div>
+    </div>
+  );
+};
+
+export default AnalyticsDashboard;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,98 @@
+// LinkShortner.js
+import React, { useState, useEffect } from 'react';
+import './dex.css'
+import { useTheme } from '../context/ThemeContext';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import '../index.css'
+
+export default function LinkShortner() {
+  const [isActive, setIsActive] = useState(false);
+  const { darkMode, toggleTheme } = useTheme();
+
+  const toggleSection = () => {
+    setIsActive(!isActive);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+  }, [darkMode]);
+  const navigationLinks = [
+    {
+      href: 'example.com',
+      iconClass: 'fa-regular fa-building',
+      text: 'Home',
+      isActive: true,
+    },
+    {
+      href: 'example.com',
+      iconClass: 'fa-regular fa-user',
+      text: 'People',
+      count: 10,
+    },
+    {
+      href: 'example.com',
+      iconClass: 'fa-regular fa-hard-drive',
+      text: 'Dashboard',
+    },
+    {
+      href: 'example.com',
+      iconClass: 'fa-regular fa-lightbulb',
+      text: 'Performance',
+      count: 20
+    },
+    {
+      href: 'example.com',
+      iconClass: 'fa-regular fa-paper-plane',
+      text: 'Development',
+    },
+    {
+      href: 'example.com',
+      iconClass: 'fa-regular fa-chart-bar',
+      text: 'Reports',
+    },
+    {
+      href: 'example.com',
+      iconClass: 'fa-regular fa-circle-user',
+      text: 'Admin',
+    },
+    {
+      href: 'example.com',
+      iconClass: 'fa-regular fa-life-ring',
+      text: 'Settings',
+    },
+  ];
+  console.log('Dark Mode:', darkMode);
+  return (
+    <div class='bod'>
+    <div className='border-r  border-gray-200'>
+  <section className={!isActive ? 'active' : ''} style={{ color: darkMode ? '#ffffff' : '#000000' }}>
+  <div className="button" onClick={toggleSection} style={{ color: darkMode ? '#ffffff' : '#000000' }}>
+    <i className="fa-solid fa-bars" />
+  </div>
+  <div className="sidebar" style={{ color: darkMode ? '#ffffff' : '#000000' }}>
+    <ul style={{ color: darkMode ? '#ffffff' : '#000000' }} >
+      {navigationLinks.map((link, index) => (
+        <li key={index} style={{ color: darkMode ? '#ffffff' : '#000000' }}>
+          <a href={link.href} className={link.isActive ? 'active' : ''}>
+            <span className="icon">
+              <i className={link.iconClass} style={{ color: darkMode ? '#ffffff' : '#000000' }}/>
+            </span>
+            <span className="item" style={{ color: darkMode ? '#ffffff' : '#000000' }}>
+                      {link.text}
+                    </span>
+            {link.count && <span className="count">{link.count}</span>}
+            
+          </a>
+          
+        </li>
+      ))}
+    </ul>
+    </div>
+</section>
+</div>
+<button >
+        <BsFillMoonStarsFill onClick={toggleTheme} className='cursor-pointer text-xl ml-10' />
+      </button>
+</div>
+  )
+}
